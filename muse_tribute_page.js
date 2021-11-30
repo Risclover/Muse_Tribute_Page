558f39c1270a8caa10e8843113c7cb9ed35885df
Risclover <85785443+Risclover@users.noreply.github.com>
2021-11-30 05:15:57 -0800

    Edited stuff

--- muse_tribute_page.js
+++ muse_tribute_page.js
@@ -2,6 +2,22 @@ const thisYear = new Date().getFullYear();
 const mattsNewAge = document.querySelector('.mattage')
 const chrisNewAge = document.querySelector('.chrisage');
 const domNewAge = document.querySelector('.domage')
+const tracksShown = document.getElementById('showtracks');
+const tracksShown2 = document.getElementById('showtracks2');
+const tracksShown3 = document.getElementById('showtracks3');
+const tracksShown4 = document.getElementById('showtracks4');
+const tracksShown5 = document.getElementById('showtracks5');
+const tracksShown6 = document.getElementById('showtracks6');
+const tracksShown7 = document.getElementById('showtracks7');
+const tracksShown8 = document.getElementById('showtracks8');
+const ohEll = document.getElementById('tracksshown');
+const ohEll2 = document.getElementById('tracksshown2');
+const ohEll3 = document.getElementById('tracksshown3');
+const ohEll4 = document.getElementById('tracksshown4');
+const ohEll5 = document.getElementById('tracksshown5');
+const ohEll6 = document.getElementById('tracksshown6');
+const ohEll7 = document.getElementById('tracksshown7');
+const ohEll8 = document.getElementById('tracksshown8');
 
 function calculate_age(dob) { 
     var diff_ms = Date.now() - dob.getTime();
@@ -22,6 +38,95 @@ function museAges() {
 museAges();
 
 console.log(calculate_age(new Date(1978, 11, 4)));
+
+tracksShown.onclick = () => showTracks(); 
+tracksShown2.onclick = () => showTracks2(); 
+tracksShown3.onclick = () => showTracks3(); 
+tracksShown4.onclick = () => showTracks4(); 
+tracksShown5.onclick = () => showTracks5(); 
+tracksShown6.onclick = () => showTracks6(); 
+tracksShown7.onclick = () => showTracks7(); 
+tracksShown8.onclick = () => showTracks8(); 
+
+function showTracks() {
+  if (ohEll.style.display === "block") {
+  ohEll.style.display = "none";
+  tracksShown.innerHTML = "Show tracks"
+  } else {
+    ohEll.style.display = "block";
+    tracksShown.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks2() {
+  if (ohEll2.style.display === "block") {
+    ohEll2.style.display = "none";
+    tracksShown2.innerHTML = "Show tracks"
+  } else {
+    ohEll2.style.display = "block";
+    tracksShown2.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks3() {
+  if (ohEll3.style.display === "block") {
+    ohEll3.style.display = "none";
+    tracksShown3.innerHTML = "Show tracks"
+  } else {
+    ohEll3.style.display = "block";
+    tracksShown3.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks4() {
+  if (ohEll4.style.display === "block") {
+    ohEll4.style.display = "none";
+    tracksShown4.innerHTML = "Show tracks"
+  } else {
+    ohEll4.style.display = "block";
+    tracksShown4.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks5() {
+  if (ohEll5.style.display === "block") {
+    ohEll5.style.display = "none";
+    tracksShown5.innerHTML = "Show tracks"
+  } else {
+    ohEll5.style.display = "block";
+    tracksShown5.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks6() {
+  if (ohEll6.style.display === "block") {
+    ohEll6.style.display = "none";
+    tracksShown6.innerHTML = "Show tracks"
+  } else {
+    ohEll6.style.display = "block";
+    tracksShown6.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks7() {
+  if (ohEll7.style.display === "block") {
+    ohEll7.style.display = "none";
+    tracksShown7.innerHTML = "Show tracks"
+  } else {
+    ohEll7.style.display = "block";
+    tracksShown7.innerHTML = "Hide tracks"
+  }
+}
+
+function showTracks8() {
+  if (ohEll8.style.display === "block") {
+    ohEll8.style.display = "none";
+    tracksShown8.innerHTML = "Show tracks"
+  } else {
+    ohEll8.style.display = "block";
+    tracksShown8.innerHTML = "Hide tracks"
+  }
+}
 
 //Get the button:
 mybutton = document.getElementById("myBtn");
@@ -44,3 +149,4 @@ function topFunction() {
 }
 
 window.onscroll = function() {scrollFunction()};
+
